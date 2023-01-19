4429cb88a7882350c0458121e6c8c2b8c873f6ec
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-20 01:54:42 +0530

    Added: Day8 Employee Wage

---
+++ src/com/bridgelabz/day8employeewage/EmployeeWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.day8employeewage;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
