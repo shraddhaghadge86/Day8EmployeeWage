8d02212af6812c27d9a823e59b4d4066b855aa68
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-01-22 14:18:28 +0530

    Added: Part Time employee Wage

--- src/com/bridgelabz/day8employeewage/EmployeeWage.java
+++ src/com/bridgelabz/day8employeewage/EmployeeWage.java
@@ -2,19 +2,23 @@ package com.bridgelabz.day8employeewage;
 
 public class EmployeeWage {
 
-    static final int Wage_Per_Hour = 20;
-    static final int Full_Time_Hour = 8;
-    public static void main(String[] args) {
-        int employeeAttendance = (int)Math.round(Math.random());
-        System.out.println(employeeAttendance);
-        int dailyWage = 0;
-        if (employeeAttendance == 1){
-            System.out.println("Employee is Present");
-            dailyWage = Wage_Per_Hour*Full_Time_Hour;
+        static final int Wage_Per_Hour = 20;
+        static final int Full_Time_Hour = 8;
+        static final int Part_Time_Hour = 4;
+        public static void main(String[] args) {
+            int employeeAttendance=(int)Math.floor(Math.random()*10)%3;
+            System.out.println(employeeAttendance);
+            int dailyWage = 0;
+
+            if (employeeAttendance == 1){
+                System.out.println("Employee is Present");
+                dailyWage = Wage_Per_Hour*Full_Time_Hour;
+            } else if (employeeAttendance == 2) {
+                dailyWage = Wage_Per_Hour*Part_Time_Hour;
+            } else {
+                System.out.println("Employee is absent");
+            }
+
+            System.out.println("daily Wage: "+dailyWage);
         }
-        else {
-            System.out.println("Employee is absent");
-        }
-        System.out.println("daily Wage: "+dailyWage);
-    }
 }
