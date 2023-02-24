bf1dd8a0ffa29693ecbb159a2f0a7be21ef4e5c7
shraddhaghadge86 <shraddhaghadge86@gmail.com>
2023-02-25 01:29:59 +0530

    Added: Employee Wage Problem

--- src/com/bridgelabz/day8employeewage/CompanyEmpWage.java
+++ src/com/bridgelabz/day8employeewage/CompanyEmpWage.java
@@ -1,7 +1,52 @@
 package com.bridgelabz.day8employeewage;
 
 public class CompanyEmpWage {
-        String companyName;
-        int totalWage;
+    String companyName;
+    int totalWage;
+
+    CompanyEmpWage(String name,int wagePerHour, int workingHouirsInMonth, int workingDaysInMonth){
+        this.wagePerHour = wagePerHour;
+        this.workingHouirsInMonth = workingHouirsInMonth;
+        this.workingDaysInMonth = workingDaysInMonth;
+        this.companyName = name;
+    }
+
+    static final int FULL_DAY_HOUR = 8;
+
+    static final int PART_TIME_HOUR = 4;
+
+    static final int FULL_TIME_EMPLOYEE = 2;
+    static final int PART_TIME_EMPLOYEE = 1;
+
+    int workingHouirsInMonth;
+    int wagePerHour;
+    int workingDaysInMonth;
+
+    void getTotalWage(){
+        int wage = 0;
+        int totalwage = 0;
+        int totalHours = 0;
+        int totaldays = 1;
+
+        while(totaldays <=this.workingDaysInMonth && totalHours<=this.workingHouirsInMonth){
+            int employeeCheck = (int) Math.floor(Math.random()*10) %3;
+            switch(employeeCheck){
+                case FULL_TIME_EMPLOYEE:
+                    wage = FULL_DAY_HOUR * this.wagePerHour;
+                    totalHours+= FULL_DAY_HOUR;
+                    break;
+                case PART_TIME_EMPLOYEE:
+                    wage = PART_TIME_HOUR * this.wagePerHour;
+                    totalHours+= PART_TIME_HOUR;
+                    break;
+                default:
+                    wage = 0;
+            }
+            totalwage = totalwage+wage;
+            totaldays++;
+        }
+
+        System.out.println(totalwage);
+    }
     }
 
